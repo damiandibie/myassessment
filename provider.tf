50e46cfa58162d3a36073071fb5aae4d68eeddb7
damiandibie <105667812+damiandibie@users.noreply.github.com>
2024-10-20 13:27:43 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "3.74.0"
     }
   }
 }
@@ -10,8 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAU6VTTJG4M6TAX7UR"
-  secret_key = "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"
 }
 
 provider "aws" {
