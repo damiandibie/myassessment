6b25dccea330fc76e48c805d00a74cbdd4f647b5
damiandibie <105667812+damiandibie@users.noreply.github.com>
2024-10-21 11:58:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -159,12 +159,12 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "8.0"
   instance_class       = var.db_instance_class
   db_name              = var.db_name
   username             = var.db_username
   password             = var.db_password
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mysql8.0"
   db_subnet_group_name = aws_db_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   skip_final_snapshot  = true
