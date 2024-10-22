b4823d883bea1af3ac18a8c430836a2716f56a8a
damiandibie <105667812+damiandibie@users.noreply.github.com>
2024-10-22 16:11:18 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "damiand-s3" {
-   default = "damiand-assess-bucket"
+  default = "damiand-assess-bucket"
 }
 variable "db_username" {
-   
+
 }
 variable "db_password" {
-   
+
 }
 
 variable "vpc_cidr" {
@@ -26,4 +26,22 @@ variable "instance_type" {
   description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
+}
+variable "dam-sg-ec2" {
+  default     = "dam-sg-ec2"
+}
+variable "dam-ie-ec2" {
+  default     = "dam-ie-ec2"
+}
+variable "dam-sg-lb" {
+  default     = "dam-sg-lb"
+}
+variable "dam-sg-alb-sg" {
+  default     = "dam-sg-alb-sg"
+}
+variable "dam-ie-asg" {
+  default     = "dam-ie-asg"
+}
+variable "ec2-template-ie" {
+  default     = "ec2-template-ie"
 }
