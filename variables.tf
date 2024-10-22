87c37e78005f24cef3d24797bb0e8bdef0ffde0b
damiandibie <105667812+damiandibie@users.noreply.github.com>
2024-10-22 15:36:14 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,3 +22,8 @@ variable "ie_subnet_cidr" {
   description = "CIDR block for the subnet in Singapore"
   default     = "10.0.1.0/24"
 }
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
